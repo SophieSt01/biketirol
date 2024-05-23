d0dae7eca445d11880ffb6f45196a603cb7b5bfc
SophieSt01 <sophie.stoffl@uibk.ac.at>
2024-05-23 17:10:15 +0200

    versuchszeile option aus index.html löschen

--- main.js
+++ main.js
@@ -68,5 +68,5 @@ controlElevation.load("data/etappe14.gpx"); //ruft plugin auf
 
 let pulldown = document.querySelector("#pulldown"); 
 for (let etappe of ETAPPEN){
-    pulldown.innerHTML +=`<option value="${etappe.user}">${etappe.titel}</option>`
+    pulldown.innerHTML +=`<option value="${etappe.user}">Etappe ${etappe.nr}: ${etappe.titel}</option>`
 }
